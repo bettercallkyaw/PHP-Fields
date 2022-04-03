540d0652da4bb480924be246da1c656168953d3f
bettercallkyaw <bettercallkyaw@outlook.com>
2022-04-03 09:34:24 +0630

    superglobal_variables

---
+++ PHP Fundamental/superglobal_variables.php
@@ -0,0 +1,43 @@
+<?php 
+
+echo $_SERVER['HTTP_USER_AGENT']."<hr>";
+
+echo $_SERVER['QUERY_STRING'] . "<hr>";
+
+echo $_SERVER['PHP_SELF'] . "<hr>";
+
+echo $_SERVER['SERVER_ADDR'] . "<hr>";
+
+echo $_SERVER['SERVER_NAME'] . "<hr>";
+
+echo $_SERVER['REQUEST_METHOD'] . "<hr>";
+
+echo $_SERVER['SERVER_PROTOCOL'] . "<hr>";
+
+echo $_SERVER['REQUEST_TIME'] . "<hr>";
+
+echo $_SERVER['DOCUMENT_ROOT'] . "<hr>";
+
+echo $_SERVER['HTTP_HOST'] . "<hr>";
+
+echo $_SERVER['SCRIPT_FILENAME'] . "<hr>";
+
+echo $_SERVER['REQUEST_URI'] . "<hr>";
+
+echo $_SERVER['REMOTE_ADDR'] . "<hr>";
+
+echo $_SERVER['GATEWAY_INTERFACE'] . "<hr>";
+
+echo $_SERVER['HTTP_ACCEPT'] . "<hr>";
+
+echo $_SERVER['HTTP_ACCEPT_ENCODING'] . "<hr>";
+
+echo $_SERVER['SERVER_SOFTWARE'] . "<hr>";
+
+echo $_SERVER['HTTP_CONNECTION'] . "<hr>";
+
+echo $_SERVER['SERVER_PORT'] . "<hr>";
+
+echo $_SERVER['SERVER_ADMIN'] . "<hr>";
+
+echo $_SERVER['SERVER_SIGNATURE'] . "<hr>";
