186e1db401b67901af3a9a6e9c07ef98799944f4
bettercallkyaw <bettercallkyaw@outlook.com>
2022-04-03 08:49:44 +0630

    comments

---
+++ PHP Fundamental/comments.php
@@ -0,0 +1,21 @@
+<?php
+
+//single line comment
+# This is also a single-line comment
+//echo 'Hello World';
+
+//---------------------------------------------
+
+//multiple line comment
+/*
+echo '<br>';
+echo 'Goodbye World';
+echo '<br>';
+echo 'I want to be A pro in PHP';
+ */
+
+//----------------------------------------------
+
+// You can also use comments to leave out parts of a code line
+$x = 4/*+5*/ + 6;
+echo $x;
